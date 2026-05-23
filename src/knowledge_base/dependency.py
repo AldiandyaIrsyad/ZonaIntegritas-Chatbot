ab123123ff84ea1859aa3acedd59b40a642e42fd
Aldiandya Irsyad <dragonopy@gmail.com>
2026-05-23 22:24:53 +0700

    initial commit

--- src/knowledge_base/dependency.py
+++ src/knowledge_base/dependency.py
@@ -3,9 +3,16 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.core.database import get_db
 from src.knowledge_base.repository import PDFRepository
 from src.knowledge_base.service import KnowledgeBase
+from src.infra.storage import StorageProvider, LocalStorageProvider
+
+def get_storage_provider() -> StorageProvider:
+    return LocalStorageProvider()
 
 def get_pdf_repository(db: AsyncSession = Depends(get_db)) -> PDFRepository:
     return PDFRepository(db)
 
-def get_pdf_service(repository: PDFRepository = Depends(get_pdf_repository)) -> KnowledgeBase:
-    return KnowledgeBase(repository=repository)
+def get_pdf_service(
+    repository: PDFRepository = Depends(get_pdf_repository),
+    storage: StorageProvider = Depends(get_storage_provider)
+) -> KnowledgeBase:
+    return KnowledgeBase(repository=repository, storage=storage)
